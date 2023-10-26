a0e1c0814e9090859917d0280855dd96cd714c2f
Your Name <you@example.com>
2023-10-26 13:57:59 +0100

    jenkins script

---
+++ jenkins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install Jenkins
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y openjdk-11-jre
+sudo curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key \
+        | sudo tee /usr/share/keyrings/jenkins-keyring.asc > \
+        /dev/null
+ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+        https://pkg.jenkins.io/debian binary/ | \
+        sudo tee /etc/apt/sources.list.d/jenkins.list > \
+        /dev/null
+ sudo apt-get update
+ sudo apt-get install -y jenkins
+ sudo apt-get install -y  git docker.io
